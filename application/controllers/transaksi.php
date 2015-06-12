e1833babb8a34746508e09e56924ccc910e638d1
11diinannisa <diinaitsnaannisa@gmail.com>
2015-06-12 19:50:44 +0700

    Reservasi Gedung, versi 1.6

--- application/controllers/transaksi.php
+++ application/controllers/transaksi.php
@@ -23,15 +23,6 @@ class Transaksi extends CI_Controller
 			$gedung = $this->gedung_m->selectBy('kd_gedung', $row->kd_gedung)->row();
 			$tipe_sewa = $this->tipe_sewa_m->selectBy('kd_tipe_sewa', $row->kd_tipe_sewa)->row();
 
-			if ($row->lunas==1)
-            {
-            	$row->lunas = 'lunas';
-            }
-            else
-            {
-            	$row->lunas = 'belum lunas';
-            }
-
 			$data['data'] .= "<tr>
                                 <td>$no.</td>
                                 <td>$row->kd_booking_gedung</td>
@@ -44,7 +35,7 @@ class Transaksi extends CI_Controller
                                 <td>$row->jumlah_tamu</td>
                                 <td>$row->keterangan</td>
                                 <td>$row->lunas</td>
-                                <td><a href='".base_url()."penyewa/view/$row->kd_data_penyewa'><font color='blue'>Update</a></font></td>					
+                                <td><a href='".base_url()."transaksi/view/$row->kd_booking_gedung'><font color='blue'>Update</a></font></td>					
                                 
                             </tr>";
 
@@ -58,9 +49,55 @@ class Transaksi extends CI_Controller
 
 	function view()
 	{
+		$id = $this->uri->segment(3);
+		$transaksi = $this->transaksi_m->selectAll();
+		$data['data'] = "";
+		
+		foreach ($transaksi as $row)
+		{
+			$penyewa = $this->penyewa_m->selectBy('kd_data_penyewa', $row->kd_data_penyewa)->row();
+			$gedung = $this->gedung_m->selectBy('kd_gedung', $row->kd_gedung)->row();
+			$tipe_sewa = $this->tipe_sewa_m->selectBy('kd_tipe_sewa', $row->kd_tipe_sewa)->row();
+
+			$data['data'] .= "<tr>
+                                <td>$row->kd_booking_gedung</td>
+                                <td>$penyewa->nama_penyewa</td>
+                                <td>$gedung->nama_gedung</td>
+                                <td>$tipe_sewa->nama_tipe_sawa</td>
+                                <td>$row->tanggal_booking</td>
+                                <td>$row->tanggal_sewa</td>
+                                <td>$row->durasi</td>
+                                <td>$row->jumlah_tamu</td>
+                                <td>$row->keterangan</td>
+                                <td>$row->lunas</td>
+                                <td><a href='".base_url()."transaksi/view/$row->kd_booking_gedung'><font color='blue'>Update</a></font></td>					
+                                
+                            </tr>";
+		}
+
+		if ($this->input->post('update'))
+		{
+			$kd_booking_gedung = $this->input->post('kd_booking_gedung');
+			$data = array('kd_booking_gedung' => $this->input->post('kd_booking')
+						  ,'kd_data_penyewa' => $this->input->post('kd_penyewa')
+						  ,'kd_gedung' => $this->input->post('gedung')
+						  ,'kd_tipe_sewa' => $this->input->post('tipe_sewa')
+						  ,'tanggal_booking' => $this->input->post('tanggal_booking')
+						  ,'tanggal_sewa' => $this->input->post('tanggal_sewa')
+						  ,'durasi' => $this->input->post('durasi')
+						  ,'jumlah_tamu' => $this->input->post('jumlah_tamu')
+						  ,'keterangan' => $this->input->post('keterangan')
+						  ,'lunas' => $this->input->post('lunas'));
+			$this->transaksi_m->update($data, kd_booking_gedung);
+			// $this->penyewa_m->update($data, $kd_penyewa);
+			redirect(base_url().'transaksi');
+			
+		}
+
 		$data['title'] = 'Transaksi';
-		$data['main_view'] = 'transaksi_v';
+		$data['main_view'] = 'transaksi_update_v';
 		$this->load->view('templatemenu_v', $data);
+
 	}
 	
 }
---
+++ application/views/transaksi_update_v.php
@@ -0,0 +1,132 @@
+
+
+    <!-- Header -->
+    <header id="home">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="intro-text">
+                        <span class="skills"><strong>UPDATE DATA PENYEWA</strong></span>
+                    </div>
+                    <br><br>
+                    <div class="tabel">
+                    <form action="" method="post" role="form" action="<?php echo base_url(); ?>transaksi">
+                        <table border="0" class="table-responsive">
+                            <tr >
+                                <td>Kode Penyewa</td>
+                                <td>:&nbsp;&nbsp;</td>
+                                <td><textarea class="form-controlx" name="kd_penyewa" type="text" required><?php echo $kd_data_penyewa; ?></textarea></td>
+                                <td width="20"></td>
+                                <td>Kode Booking&nbsp;&nbsp;</td>
+                                <td>:&nbsp;&nbsp;</td>
+                                <td><input class="form-controlx" name="kd_booking" type="text" required>
+                                    
+                                 </td>
+                            </tr>
+                            <tr >
+                                <td>Nama</td>
+                                <td>:&nbsp;&nbsp;</td>
+                                <td><textarea disabled="true" class="form-controlx" name="nama_penyewa" type="text" required><?php echo $nama_penyewa; ?></textarea></td>
+                                <td width="20"></td>
+                                <td>Kode Gedung&nbsp;&nbsp;</td>
+                                <td>:&nbsp;&nbsp;</td>
+                                <td><select name="gedung">
+                                    <?php
+                                        foreach ($gedung as $row)
+                                        {
+                                            echo "<option value='$row->kd_gedung'>$row->nama_gedung</option>";
+                                        }
+                                    ?>
+                                    </select></td>
+                            </tr>
+                            <tr>
+                                <td >Alamat</td>
+                                <td>:&nbsp;&nbsp;</td>
+                                <td><textarea disabled="true" class="form-controlx" name="alamat" type="text" required><?php echo $alamat; ?></textarea></td>
+                                <td width="20"></td>
+                                <td>Kode Sewa&nbsp;&nbsp;</td>
+                                <td>:&nbsp;&nbsp;</td>
+                                
+                                <td><select name="tipe_sewa">
+                                    <?php
+                                        foreach ($tipe_sewa as $row)
+                                        {
+                                            echo "<option value='$row->kd_tipe_sewa'>$row->nama_tipe_sawa</option>";
+                                        }
+                                    ?>
+                                    </select>
+                                </td>
+                            </tr>
+                            
+                            <tr>
+                                <td >Telepon&nbsp;&nbsp;</td>
+                                <td>:&nbsp;&nbsp;</td>
+                                <td><textarea disabled="true" class="form-controlx" name="no_telp" type="text" required><?php echo $no_telp; ?></textarea></td>
+                                <td width="20"></td>
+                                <td>Harga&nbsp;&nbsp;</td>
+                                <td>:&nbsp;&nbsp;</td>
+                                <td><input class="form-controlx" name="harga" type="text" required></td>
+                            </tr>
+
+                            <tr>
+                                <td >Tanggal Sewa&nbsp;&nbsp;</td>
+                                <td>:&nbsp;&nbsp;</td>
+                                <td><input class="form-controlx datepicker" name="tanggal_booking" type="text" required></td>
+                                <td width="20"></td>
+                                <td >Durasi&nbsp;&nbsp;</td>
+                                <td>:&nbsp;&nbsp;</td>
+                                <td><input class="form-controlx" name="durasi" type="text" required value="1"></td>
+                            </tr>
+                            <tr>
+                                <td>Tanggal Booking&nbsp;&nbsp;</td>
+                                <td>:&nbsp;&nbsp;</td>
+                                <td><input class="form-controlx" name="tanggal_sewa" type="text" required value="<?php echo date('Y-m-d H:i:s ', time()+60*60*5); ?>"></td>
+                                
+                                <td width="20"></td>
+                                <td>Jumlah Tamu&nbsp;&nbsp;</td>
+                                <td>:&nbsp;&nbsp;</td>
+                                <td>
+                                    <select name="jumlah_tamu">
+                                    <option value="100"> 100 orang</option>
+                                    <option value="150"> 150 orang</option>
+                                    <option value="200"> 200 orang</option>
+                                    <option value="250"> 250 orang</option>
+                                    </select>
+                                    <!-- <input class="form-controlx" name="jumlah_tamu" type="text" required> -->
+                                </td>
+                            </tr>
+                            <tr>
+                                <td >Keterangan&nbsp;&nbsp;</td>
+                                <td>:&nbsp;&nbsp;</td>
+                                <td><input class="form-controlx" name="keterangan" type="text" required></td>
+                                <td width="20"></td>
+                                <td>Lunas&nbsp;&nbsp;</td>
+                                <td>:&nbsp;&nbsp;</td>
+                                <td><select name="lunas">
+                                    <option value="0">Belum Lunas</option>
+                                    <option value="1">Lunas</option>
+                                    </select>
+                                    <!-- <input class="form-controlx" name="lunas" type="text" required> --></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td><br><input type="submit" class="btn btn-primary" name="add" id="submit" value="PROSES"></td>
+                            </tr>
+                            
+                        </table>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <script>
+  $(function() {
+    $( ".datepicker" ).datepicker(
+        {
+            dateFormat:'yy-mm-dd'
+        });
+  });
+  </script>
